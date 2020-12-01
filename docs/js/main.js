baa0585772eab19f0bff669b51144fda6ca4b86b
arizel022 <arizel022@gmail.com>
2020-12-01 19:39:42 +0100

    add header, menu

---
+++ docs/js/main.js
@@ -0,0 +1,7 @@
+$('.header__menu').on('click', function(){
+    $('.menu__list').slideToggle();
+});
+
+// $('.menu__trigger').on('hover', function(){
+//     $('.menu__list2').slideToggle();
+// });
