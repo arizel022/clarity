7fcdf5a1fec6593fd5cc6fc2044971847c06b863
arizel022 <arizel022@gmail.com>
2020-12-04 20:43:59 +0100

    form v1.0

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,40 @@
-$('.header__menu').on('click', function(){
-    $('.menu__list').slideToggle();
+$(document).ready(function () {
+    $('.menu__list').hide();
+
+    $('.header__menu').on('click', function () {
+        $('.menu__list').slideToggle();
+    });
+
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<div class="sl__arrow sl__arrow--prev icon-long-arrow-up"></div>',
+        nextArrow: '<div class="sl__arrow sl__arrow--next icon-long-arrow-down"></div>',
+        // autoplay: true,
+        autoplaySpeed: 3000,
+        vertical: true,
+        verticalSwiping: true,
+        dots: true,
+        dotsClass: "sl__dots"
+    });
+
+
+    	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 });
 
-// $('.menu__trigger').on('hover', function(){
-//     $('.menu__list2').slideToggle();
-// });
